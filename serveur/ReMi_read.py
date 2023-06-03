a42d28b0762b2b25ec5ccdd3ba19bf9e4c680799
joanix2 <j.dussauld@orange.fr>
2023-06-03 15:41:29 +0200

    update serveur

--- serveur/ReMi_read.py
+++ serveur/ReMi_read.py
@@ -1,56 +1,68 @@
 from pythonosc import dispatcher
 from pythonosc import osc_server
+from pythonosc import udp_client
 
-# Adresse IP et port sur lesquels écouter les messages OSC
-ip = "127.0.0.1"
-port = 9000
+# Adresse IP et port du client "send.py"
+send_ip = "127.0.0.1"
+send_port = 8000
 
-def new_reservoir(*args):
-    print(args)
-    taille = args[0]
-    leak_rate = args[1]
-    spectral_radius = args[2]
-    input_scaling = args[3]
+# Création du client OSC pour envoyer des messages à "send.py"
+client = udp_client.SimpleUDPClient(send_ip, send_port)
+
+note_set = {}
+
+def process_message(func):
+    def wrapper(*args):
+        try:
+            func(*args)
+        except Exception as error:
+            print(f"Error 400: Invalid Request : {error}")
+    return wrapper
+
+@process_message
+def set_reservoir(address, *args):
+    key = args[0]
+    value = args[1]
     
     # Faire quelque chose avec les paramètres reçus
-    print(f"Taille : {taille}")
-    print(f"Leak Rate : {leak_rate}")
-    print(f"Spectral Radius : {spectral_radius}")
-    print(f"Input Scaling : {input_scaling}") 
+    print(f"key : {key}")
+    print(f"Value: {value}")
 
-def reset_reservoir(*args):
-    print("Reçu un message OSC pour réinitialiser")
-
-def get_reservoir(*args):
-    print("get note")
+@process_message
+def update_note(address, *args):
+    pitch = args[0]
+    velocity = args[1]
     
-links = {
-"/reservoir/new" : new_reservoir,
-"/reservoir/reset" : reset_reservoir,
-"/reservoir/get" : get_reservoir,
-}
+    if velocity == 0:
+        note_set.remove(pitch)
+    else :
+        note_set.add(pitch)
+    
+    # Faire quelque chose avec les paramètres reçus
+    print(f"Pitch : {pitch}")
+    print(f"Velocity : {velocity}")
+    print(f"Note set : {note_set}")
 
-def process_message(address, *args):
-    not_find = True
-    for l in links:
-        if (l == address):
-            try :
-                not_find = False
-                links[l](args)
-                break;
-            except Exception as error:
-                print(f"Error 400: Invalid Request : {error}")
-    if not_find :
-        print(f"Adresse OSC non reconnue : {address}")
+@process_message
+def get_note(address, *args):
+    print("get note")
+    client.send_message("/", note_set)
 
-# Création du dispatcher et ajout de la fonction de traitement
-dispatcher = dispatcher.Dispatcher()
-for l in links:
-    dispatcher.map(l, process_message)
+if __name__ == "__main__":
+    # Adresse IP et port sur lesquels écouter les messages OSC
+    ip = "127.0.0.1"
+    port = 9000
+    
+    # Création du dispatcher et ajout de la fonction de traitement
+    dispatcher = dispatcher.Dispatcher()
+    dispatcher.map("/reservoir/set_reservoir", set_reservoir)
+    dispatcher.map("/reservoir/update_note", update_note)
+    dispatcher.map("/reservoir/get_note", get_note)
+    
+    # Création du serveur OSC
+    server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
+    
+    # Démarrage du serveur
+    print(f"Écoute des messages OSC sur {ip}:{port}")
+    server.serve_forever()
 
-# Création du serveur OSC
-server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
-
-# Démarrage du serveur
-print(f"Écoute des messages OSC sur {ip}:{port}")
-server.serve_forever()
