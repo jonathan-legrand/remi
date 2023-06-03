7bd770dcba01203d268cd27d84f8c58f86cfe5be
joanix2 <j.dussauld@orange.fr>
2023-06-03 18:50:23 +0200

    interface

--- serveur/ReMi_interface.py
+++ serveur/ReMi_interface.py
@@ -8,44 +8,90 @@ Created on Sat Jun  3 12:39:52 2023
 
 import tkinter as tk
 from pythonosc import udp_client
+from pythonosc import osc_server
+from pythonosc import dispatcher
 
-# Adresse IP et port du destinataire OSC
-ip = "127.0.0.1"
-port = 9000
+class FloatEntry(tk.Frame):
+    def __init__(self, parent, label_text):
+        super().__init__(parent)
+        
+        # Create label
+        self.label = tk.Label(self, text=label_text)
+        self.label.pack(side="left")
+        
+        # Create entry
+        self.entry = tk.Entry(self)
+        self.entry.pack(side="left")
+    
+    def get_entry_value(self):
+        return self.entry.get()
+    
+class Ware(tk.Tk):
+    def __init__(self, ip, send_port, receive_port):
+        super().__init__()
+        
+        # Création du dispatcher et ajout de la fonction de traitement
+        self.dispatcher = dispatcher.Dispatcher()
+        #self.dispatcher.map("/reservoir/set_reservoir", set_reservoir)
+            
+        # Création du serveur OSC
+        #server = osc_server.ThreadingOSCUDPServer((ip, port), self.dispatcher)
+        
+        # Création du client OSC
+        self.client = udp_client.SimpleUDPClient(ip, send_port)
+        
+        # Create a frame to hold the widgets
+        self.frame = tk.Frame(self)
+        self.frame.pack(anchor="w", padx=10, pady=10)  # Right alignment with padding
+        
+        # Création des boutons
+        self.pitch_entry = FloatEntry(self.frame, "Pitch")
+        self.velocity_entry = FloatEntry(self.frame, "Velocity")
+        self.update_note_button = tk.Button(self.frame, text="Update note", command=self.update_note_clicked)
+        self.key_entry = FloatEntry(self.frame, "key")
+        self.value_entry = FloatEntry(self.frame, "value")
+        self.set_reservoir_button = tk.Button(self.frame, text="Set reservoir", command=self.set_reservoir_clicked)
+        self.get_button = tk.Button(self.frame, text="Get", command=self.get_button_clicked)
+        
+        # Placement des boutons dans le frame
+        self.pitch_entry.pack(anchor="w")
+        self.velocity_entry.pack(anchor="w")
+        self.update_note_button.pack()
+        self.key_entry.pack(anchor="w")
+        self.value_entry.pack(anchor="w")
+        self.set_reservoir_button.pack()
+        self.get_button.pack()
+    
+    def update_note_clicked(self):
+        pitch = int(self.pitch_entry.get_entry_value())
+        velocity = int(self.velocity_entry.get_entry_value())
+        address = "/reservoir/update_note"
+        args = (pitch, velocity)  # Exemple d'argument
+        self.client.send_message(address, args)
+        
+    def set_reservoir_clicked(self):
+        # N 1 a 10000
+        # lr 10-3 a 1
+        # sr 10-3 1000
+        # input_scaling 10-3 1000
+        lr = self.key_entry.get_entry_value()
+        sr = float(self.value_entry.get_entry_value())
+        # Use lr and sr values for processing
+        address = "/reservoir/set_reservoir"
+        args = (lr, sr)  # Exemple d'argument
+        self.client.send_message(address, args)
+        
+    def get_button_clicked(self):
+        # Perform some action when the Get button is clicked
+        pass
+    
+if __name__ == "__main__":
+    # Adresse IP et port du destinataire OSC
+    ip = "127.0.0.1"
+    send_port = 9000
+    receive_port = 8000
+    # Création de la fenêtre principale
+    window = Ware(ip, send_port, receive_port)
+    # Démarrage de la boucle principale de la fenêtre
+    window.mainloop()
 
-# Création du client OSC
-client = udp_client.SimpleUDPClient(ip, port)
-
-def new_button_clicked(taille = 100, lr=0.5, sr=0.9, ic=0.7):
-    # Envoi du message OSC
-    address = "/reservoir/new"
-    args = (taille, lr, sr, ic)  # Exemple d'argument
-    client.send_message(address, args)
-
-def reset_button_clicked():
-    # Envoi du message OSC
-    address = "/reservoir/reset"
-    args = 12345  # Exemple d'argument
-    client.send_message(address, args)
-
-def get_button_clicked():
-    # Envoi du message OSC
-    address = "/reservoir/get"
-    args = 12345  # Exemple d'argument
-    client.send_message(address, args)
-
-# Création de la fenêtre principale
-window = tk.Tk()
-
-# Création des boutons
-new_button = tk.Button(window, text="New", command=new_button_clicked)
-reset_button = tk.Button(window, text="Reset", command=reset_button_clicked)
-get_button = tk.Button(window, text="Get", command=get_button_clicked)
-
-# Placement des boutons dans la fenêtre
-new_button.pack()
-reset_button.pack()
-get_button.pack()
-
-# Démarrage de la boucle principale de la fenêtre
-window.mainloop()
